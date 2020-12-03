c4acb8ef594826e33ac398dd1fa18592b3a6b641
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-12-03 23:12:50 +0530

    Update hdb.py

--- hdb.py
+++ hdb.py
@@ -5,3 +5,24 @@ covid_data= pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-
 covid_data['Active'] = covid_data['Confirmed'] - covid_data['Deaths'] - covid_data['Recovered']
 result = covid_data.groupby('Country/Region')['Confirmed', 'Deaths', 'Recovered', 'Active'].sum().reset_index()
 print(result)
+
+
+"C:\Users\Manan K. Garg\PycharmProjects\practice\venv\Scripts\python.exe" "C:/Users/Manan K. Garg/PycharmProjects/practice/Covid19 Data Retrieval.py"
+C:/Users/Manan K. Garg/PycharmProjects/practice/Covid19 Data Retrieval.py:6: FutureWarning: Indexing with multiple keys (implicitly converted to a tuple of keys) will be deprecated, use a list instead.
+  result = covid_data.groupby('Country/Region')['Confirmed', 'Deaths', 'Recovered', 'Active'].sum().reset_index()
+                     Country/Region  Confirmed  Deaths  Recovered  Active
+0                       Afghanistan         20       0          1      19
+1                           Albania         55       1          0      54
+2                           Algeria         60       4         12      44
+3                           Andorra         39       0          1      38
+4               Antigua and Barbuda          1       0          0       1
+..                              ...        ...     ...        ...     ...
+154                         Uruguay         29       0          0      29
+155                      Uzbekistan         10       0          0      10
+156                       Venezuela         33       0          0      33
+157                         Vietnam         66       0         16      50
+158  occupied Palestinian territory          0       0          0       0
+
+[159 rows x 5 columns]
+
+Process finished with exit code 0
